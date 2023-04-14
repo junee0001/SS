be9aa592bdcd5e06307d1d07443cce4026c2da3f
june <105195601+junee0001@users.noreply.github.com>
2023-04-14 12:54:51 +1000

    Update thekey.lua

--- thekey.lua
+++ thekey.lua
@@ -1,2 +1,4 @@
 thekey = '663761340add0a16fe84499013502d30'
-print("Loaded Github Key")
+print("Loaded Github thekey")
+theultrapremiumkey = '41b6acd1c5a0657cbeb423df43d36842'
+print("Loaded Github theultrapremiumkey")
